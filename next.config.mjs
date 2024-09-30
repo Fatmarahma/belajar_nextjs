18f9d21c0c4aee51e71f98608a75d0ad4acb4987
Fatmarahma <gemmy1929@gmail.com>
2024-09-30 11:19:06 +0700

    perkenalan nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // basePath:"/home", // <- basePath untuk mengubah url halaman utama 
 };
 
 export default nextConfig;
+
+/**
+ * file next.config.js dibuat otomatis oleh nextjs di root folder untuk ngatur konfigurasi nextjs
+ * lebih detailnya bisa baca dokumentasinya di https://nextjs.org/docs/pages/api-reference/next-config-js
+ */
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#ffffff",
+        warnaGue: "#FFB0B0",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
   plugins: [],
 };
+
+/**
+ * file tailwind.config.js file yang  dibuat otomatis oleh nextjs saat instalasi yang  digunakan
+ * untuk mengkonfigurasi tailwinddcss seperti warna custom,gradient, ukuran,dll
+ * lebih detailnya bisa baca dikumentasinya di https://tailwindcss.com/docs/configuration
+ */
